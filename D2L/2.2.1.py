201825508640c71326339f0981f048174c244763
Song <vvright@163.com>
2021-02-12 16:59:09 +0800

    step forward

--- D2L/2.2.1.py
+++ D2L/2.2.1.py
@@ -3,5 +3,14 @@ import torch
 x = torch.empty(5, 3)
 print(x)
 
-y = torch.rand(5, 3)
-print(y)
+x = torch.rand(5, 3)
+print(x)
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+
+x = torch.tensor([5.5, 3])
+print(x)
+
+x = x.new_ones(5, 3, dtype=torch.float64)
+print(x)
